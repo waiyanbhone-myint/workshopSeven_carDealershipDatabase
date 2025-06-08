008030ff2e8ded3940089ac4478e489efd748262
Wai_Yan <waiyanbhonemyint2002@gmail.com>
2025-06-08 08:24:52 -0400

    Done

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 -- 1. Get all dealerships
 SELECT * FROM dealerships;
 
--- 2. Find all vehicles for a specific dealership (e.g., dealership_id = 1)
+-- 2. Find all vehicles for a specific dealership
 SELECT v.*
 FROM vehicles v
 JOIN inventory i ON v.VIN = i.VIN
@@ -17,7 +17,7 @@ FROM dealerships d
 JOIN inventory i ON d.dealership_id = i.dealership_id
 WHERE i.VIN = '1HGCM82633A123456';
 
--- 5. Find all dealerships that have a specific car type (e.g., Red Ford Mustang)
+-- 5. Find all dealerships that have a specific car type
 SELECT DISTINCT d.*
 FROM dealerships d
 JOIN inventory i ON d.dealership_id = i.dealership_id
@@ -28,9 +28,3 @@ WHERE v.make = 'Ford' AND v.model = 'Mustang' AND v.color = 'Red';
 SELECT * FROM sales_contracts
 WHERE dealership_id = 1
 AND sale_date BETWEEN '2024-01-01' AND '2024-12-31';
-
--- 7. (Bonus) Top 3 most expensive unsold cars
-SELECT * FROM vehicles
-WHERE sold = FALSE
-ORDER BY price DESC
-LIMIT 3;
